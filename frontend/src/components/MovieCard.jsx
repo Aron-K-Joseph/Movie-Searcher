e7ff1c7174eaad20d296cf6951c68f80760f084b
Aron-Kaniyarakath-Joseph <ajoseph2148@sdsu.edu>
2025-02-24 20:38:27 -0800

    images

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -15,7 +15,7 @@ function MovieCard({movie}) {
             </div>
         </div>
         <h3>{movie.title}</h3>
-        <p>{movie.release_date}</p>
+        <p>{movie.release_date?.split("-")[0]}</p>
 
     </div>
 }
